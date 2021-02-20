9181ac2aa35fca4ee7d28e1257f4d9cf38c21b93
Yashaswi Thannir <YashaswiThannir@users.noreply.github.com>
2021-02-19 20:38:01 -0800

    Add files via upload

---
+++ Category.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BestbuyTest
+{
+    public class Category   
+    {
+        public string id { get; set; } 
+        public string name { get; set; } 
+        public DateTime createdAt { get; set; } 
+        public DateTime updatedAt { get; set; } 
+    }
+}
---
+++ Datum.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace BestbuyTest
+{
+    public class Datum   
+    {
+        public int id { get; set; } 
+        public string name { get; set; } 
+        public string type { get; set; } 
+        public double price { get; set; } 
+        public string upc { get; set; } 
+        public int shipping { get; set; } 
+        public string description { get; set; } 
+        public string manufacturer { get; set; } 
+        public string model { get; set; } 
+        public string url { get; set; } 
+        public string image { get; set; } 
+        public DateTime createdAt { get; set; } 
+        public DateTime updatedAt { get; set; } 
+        public List<Category> categories { get; set; } 
+    }
+}
