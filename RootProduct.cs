53b09fc072a3587bb0b7e59d7e726c010c5be9bb
Yashaswi Thannir <YashaswiThannir@users.noreply.github.com>
2021-02-19 20:57:05 -0800

    Create RootProduct.cs

---
+++ RootProduct.cs
@@ -0,0 +1,12 @@
+
+using System.Collections.Generic;
+
+namespace BestbuyTest
+{
+public class RootProduct    {
+        public int total { get; set; } 
+        public int limit { get; set; } 
+        public int skip { get; set; } 
+        public List<Datum> data { get; set; } 
+    }
+}
